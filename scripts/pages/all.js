71188bc98d1f2d60e158c6692b2764ce66a97254
Wojtek1441 <motyl.motyl@interia.pl>
2018-04-10 16:03:34 +0100

    Added Menu, Section and footer
    some work done!

--- scripts/pages/all.js
+++ scripts/pages/all.js
@@ -1,8 +1,34 @@
-(function($, window, document) {
+//Hamburger Menu
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
 
+//Fixed navigation
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 61) {
+	    $(".top").css("background" , "#fff");
+      $(".logo").css("color" , "#ee0651");
+      $("#menu li").css("color" , "#ee0651");
+	  }
+	  else{
+		  $(".top").css("background" , "none");
+      $(".logo").css("color" , "#fff");
+      $("#menu li").css("color" , "#fff");
 
+	  }
+  });
+});
 
- $(function() {
-
- });
-}(window.jQuery, window, document));
+//Carousel
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
